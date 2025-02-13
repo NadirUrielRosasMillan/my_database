457070103916859746afd137e7e564a5fb3a31ba
Nadir Uriel Rosas Millan <144179674+NadirUrielRosasMillan@users.noreply.github.com>
2025-02-13 17:50:38 +0000

    main

---
+++ Statements/01_select.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users;  //consulta
+
+SELECT name FROM users; //solo nombre
+
+SELECT user_id, name FROM users; //cosultamos el id y los usuarios
---
+++ Statements/02_distinct.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT * FROM users
+
+SELECT DISTINCT age FROM users // solo me da las distintas edades que hay en la tabla 
