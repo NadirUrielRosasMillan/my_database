d1a89f4bb91f7f46280defd281504d2f60dca9ae
Nadir Uriel Rosas Millan <144179674+NadirUrielRosasMillan@users.noreply.github.com>
2025-02-19 21:18:22 +0000

    main

---
+++ Databases/01_insert_into.sql
@@ -0,0 +1,5 @@
+--inserta datos en la tabla
+INSERT INTO users (name, age, init_date, email) VALUES ('monica', 15, '2025-02-08', 'monica@gmail.com');
+--primero se ponen los nombres de las columnas y luego los valores
+--si son nulos podemos no ponerlos 
+--ojo con la fecha que tiene que ser en formato 'YYYY-MM-DD'
---
+++ Databases/02_update.sql
@@ -0,0 +1,3 @@
+--actualiza los datos de la tabla
+UPDATE users SET user_id ='9' WHERE user_id = 11 -- se necesita el where para saber que fila se va a actualizar
+-- sin whwere se actualizan todos los registros y eso no es lo que queremos
