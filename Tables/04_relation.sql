45c291821f990f1403db25f5d8a5b1ab765fa9b1
Nadir Uriel Rosas Millan <144179674+NadirUrielRosasMillan@users.noreply.github.com>
2025-02-23 21:02:03 +0000

    main

---
+++ Tables/04_relation.sql
@@ -0,0 +1,9 @@
+--como hacer una relacion de una tabla con otra
+
+CREATE TABLE dni(
+     dni_id int AUTO_INCREMENT PRIMARY KEY,
+     dni_number int NOT NULL,
+     user_id int,
+     UNIQUE (dni_id),
+     FOREIGN KEY(user_id) REFERENCES users (user_id)   
+);
